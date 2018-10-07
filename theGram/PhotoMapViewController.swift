6ebf0b641d1225f9ee0ce39382c208a4062ed8af
Kenia Rioja <kdrioja@gmail.com>
2018-10-06 17:15:55 -0700

    added PhotoMapViewController

---
+++ theGram/PhotoMapViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoMapViewController.swift
+//  theGram
+//
+//  Created by user145766 on 10/6/18.
+//  Copyright © 2018 Kenia Rioja. All rights reserved.
+//
+
+import UIKit
+
+class PhotoMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
