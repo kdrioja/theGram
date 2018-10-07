9864bec68a9834b1976fc2c860d80d91b9cc974c
Kenia Rioja <kdrioja@gmail.com>
2018-10-06 18:06:43 -0700

    edited PhotoMapViewController.swift

--- theGram/PhotoMapViewController.swift
+++ theGram/PhotoMapViewController.swift
@@ -8,14 +8,45 @@
 
 import UIKit
 
-class PhotoMapViewController: UIViewController {
+class PhotoMapViewController: UIViewController,  UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        vc.sourceType = UIImagePickerController.SourceType.camera
+        self.present(vc, animated: true, completion: nil)
+        
+        //instantiate a UIImagePickerController
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            print("Camera is available 📸")
+            vc.sourceType = .camera
+        } else {
+            print("Camera 🚫 available so we will use photo library instead")
+            vc.sourceType = .photoLibrary
+        }
+        
+        //
+        
+        
+        
     }
     
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        // Get the image captured by the UIImagePickerController
+        let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        
+        // Do something with the images (based on your use case)
+        
+        // Dismiss UIImagePickerController to go back to your original view controller
+        dismiss(animated: true, completion: nil)
+    }
 
     /*
     // MARK: - Navigation
